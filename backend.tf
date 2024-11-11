2de65bd8c460a45bdd5a4a181f40ee6202539931
Ala Mani <ala.a.mani@capgemini.com>
2024-11-11 17:10:09 +0100

    first commit

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket"    # Name of your S3 bucket
-    key            = "terraform/dev.tfstate"   # Path to the state file in the bucket
-    # region         = "us-west-2"                     # The AWS region where the bucket is located
-    encrypt        = true                            # Enable encryption for the state file
-  }
-}
