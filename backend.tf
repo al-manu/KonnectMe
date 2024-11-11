e0324ab301760beba84735e7f48c92558c6227c9
Ala Mani <ala.a.mani@capgemini.com>
2024-11-11 22:17:58 +0100

    first commit

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "Kstate-bucket"    # Replace with your S3 bucket name
+    bucket         = "kstate-bucket"    # Replace with your S3 bucket name
     key            = "dev/terraform.tfstate"       # Path within the S3 bucket (can be customized for each environment)
     # region         = "us-west-2"                   # Region for the S3 bucket
     encrypt        = true
