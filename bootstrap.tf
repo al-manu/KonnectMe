71cf034baa8ac26d1b1c8321381c012f9aef29dd
Ala Mani <ala.a.mani@capgemini.com>
2024-11-11 15:52:35 +0100

    first commit

--- bootstrap.tf
+++ bootstrap.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "state_backend" {
-  bucket = "your-state-bucket-name"    # The S3 bucket name for state and lock
-  # region = "us-east-1"                  # AWS region
+  bucket = "stat11"    # The S3 bucket name for state and lock
+  region = "eu-central-1"                  # AWS region
 
   versioning {
     enabled = true  # Enable versioning for state locking
