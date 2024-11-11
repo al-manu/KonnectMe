b657f438baced4beafb3ddd95bd18ae15a9e1064
Ala Mani <ala.a.mani@capgemini.com>
2024-11-11 15:49:30 +0100

    first commit

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
     key            = "terraform/state.tfstate"  # Path inside the S3 bucket for the state file
     # region         = "us-east-1"                 # AWS Region where your bucket is located
     encrypt        = true                        # Enable encryption for the state file
-    # versioning     = true                        # Enable versioning to track changes in state (acts like a lock)
+    versioning     = true                        # Enable versioning to track changes in state (acts like a lock)
     lock           = true                        # Use S3's versioning mechanism as a lock
   }
 }
