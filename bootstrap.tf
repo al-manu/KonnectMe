da574c694083ff632b4fae172bc1cd927081938f
Ala Mani <ala.a.mani@capgemini.com>
2024-11-11 22:09:35 +0100

    first commit

--- bootstrap.tf
+++ bootstrap.tf
@@ -1,13 +1,13 @@
-resource "aws_s3_bucket" "state_backend" {
-  bucket = "Kstat_terrafrom_stata"    # The S3 bucket name for state and lock
-  # region = "eu-central-1"                  # AWS region
+# resource "aws_s3_bucket" "state_backend" {
+#   bucket = "Kstat_terrafrom_stata"    # The S3 bucket name for state and lock
+#   # region = "eu-central-1"                  # AWS region
 
-  versioning {
-    enabled = true  # Enable versioning for state locking
-  }
+#   versioning {
+#     enabled = true  # Enable versioning for state locking
+#   }
 
 
-  lifecycle {
-    prevent_destroy = true  # Prevent accidental deletion of the bucket
-  }
-}
+#   lifecycle {
+#     prevent_destroy = true  # Prevent accidental deletion of the bucket
+#   }
+# }
