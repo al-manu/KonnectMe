6d924f9102cf9b07192657ca4f3002b8381aa754
Ala Mani <ala.a.mani@capgemini.com>
2024-11-12 09:45:25 +0100

    fixing issues

--- terraform/backend_setup/s3_bucket.tf
+++ terraform/backend_setup/s3_bucket.tf
@@ -2,21 +2,22 @@ variable "environment" {
   description = "The environment name (dev, staging, prod)"
   type        = string
 }
- 
+
 variable "region" {
   description = "AWS region"
   type        = string
 }
- 
+
 provider "aws" {
   region = var.region
 }
- 
+
 # Check if the bucket already exists
 data "aws_s3_bucket" "existing" {
   bucket = "proj-${var.environment}-terraform-state"
+  # The data source will fail if the bucket does not exist
 }
- 
+
 # Create the S3 bucket if it does not already exist
 resource "aws_s3_bucket" "terraform_state" {
   count  = data.aws_s3_bucket.existing.id == "" ? 1 : 0
@@ -31,25 +32,24 @@ resource "aws_s3_bucket" "terraform_state" {
 
 # Create versioning configuration for the bucket
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-  bucket = aws_s3_bucket.terraform_state[0].bucket  # Access the first (and only) instance
+  bucket = aws_s3_bucket.terraform_state[0].bucket
 
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-# Create encryption configuration for the bucket
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-  count = data.aws_s3_bucket.existing.id == "" ? 1 : 0
-  bucket = aws_s3_bucket.terraform_state[0].bucket  # Access the first (and only) instance
+# # Create encryption configuration for the bucket
+# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+#   bucket = aws_s3_bucket.terraform_state[0].bucket
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
- 
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
+
 output "bucket_name" {
-  value = aws_s3_bucket.terraform_state[0].bucket  # Access the first (and only) instance
+  value = aws_s3_bucket.terraform_state[0].bucket
 }
