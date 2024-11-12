81ca1b9c00ed51007b38d2fdf498d0c21a9db870
Ala Mani <ala.a.mani@capgemini.com>
2024-11-12 09:06:51 +0100

    fixing issues

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "kstate-bucket"    # Replace with your S3 bucket name
-    key            = "dev/terraform.tfstate"       # Path within the S3 bucket (can be customized for each environment)
-    # region         = "us-west-2"                   # Region for the S3 bucket
-    encrypt        = true
-    acl            = "bucket-owner-full-control"
-  }
-}
