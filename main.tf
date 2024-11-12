e0e7dbf1d5deac1e1ef7b4b942cd1805bb2ed06e
Ala Mani <ala.a.mani@capgemini.com>
2024-11-12 09:30:46 +0100

    fixing issues

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "kstate-bucket"   # Replace with your S3 bucket name
-  acl    = "private"
-  region = "us-west-2"
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "kstate-bucket"   # Replace with your S3 bucket name
+#   acl    = "private"
+#   region = "us-west-2"
 
-  versioning {
-    enabled = true
-  }
+#   versioning {
+#     enabled = true
+#   }
 
-  object_lock_configuration {
-    object_lock_enabled = "Enabled"
-    rule {
-      default_retention {
-        mode = "COMPLIANCE"  # You can change to "GOVERNANCE" if you need less strict retention
-        days = 30            # Specify retention period, e.g., 30 days
-      }
-    }
-  }
-}
+#   object_lock_configuration {
+#     object_lock_enabled = "Enabled"
+#     rule {
+#       default_retention {
+#         mode = "COMPLIANCE"  # You can change to "GOVERNANCE" if you need less strict retention
+#         days = 30            # Specify retention period, e.g., 30 days
+#       }
+#     }
+#   }
+# }
--- terraform/backend_setup/s3_bucket.tf
+++ terraform/backend_setup/s3_bucket.tf
@@ -14,25 +14,29 @@ provider "aws" {
  
 # Check if the bucket already exists
 data "aws_s3_bucket" "existing" {
-  bucket = "your-project-${var.environment}-terraform-state"
+  bucket = "proj-${var.environment}-terraform-state"
 }
  
 # Create the S3 bucket if it does not already exist
 resource "aws_s3_bucket" "terraform_state" {
 count = data.aws_s3_bucket.existing.id == "" ? 1 : 0
-  bucket = "your-project-${var.environment}-terraform-state"
+  bucket = "proj-${var.environment}-terraform-state"
   acl    = "private"
- 
-  versioning {
-    enabled = true
-  }
+
  
   tags = {
     Name        = "Terraform State Bucket - ${var.environment}"
     Environment = var.environment
   }
 }
- 
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+  }
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
 count = data.aws_s3_bucket.existing.id == "" ? 1 : 0
   bucket = aws_s3_bucket.terraform_state[0].bucket
