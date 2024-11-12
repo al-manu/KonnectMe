fcdd74493f9344459aa0988349baad4107e3b534
Ala Mani <ala.a.mani@capgemini.com>
2024-11-12 09:53:46 +0100

    fixing issues

--- terraform/backend_setup/s3_bucket.tf
+++ terraform/backend_setup/s3_bucket.tf
@@ -12,42 +12,37 @@ provider "aws" {
   region = var.region
 }
 
-# Attempt to read the existing bucket (if any)
-data "aws_s3_bucket" "existing" {
-  bucket = "proj-${var.environment}-terraform-state"
-}
-
-# Create the bucket if it doesn't exist
+# Create the S3 bucket (if it does not exist)
 resource "aws_s3_bucket" "terraform_state" {
-  count  = data.aws_s3_bucket.existing.id == "" ? 1 : 0
   bucket = "proj-${var.environment}-terraform-state"
   acl    = "private"
+
   tags = {
     Name        = "Terraform State Bucket - ${var.environment}"
     Environment = var.environment
   }
 }
 
-# Create versioning configuration for the bucket
+# Create versioning configuration for the S3 bucket
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-  bucket = aws_s3_bucket.terraform_state[0].bucket
+  bucket = aws_s3_bucket.terraform_state.bucket
 
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-# # Create encryption configuration for the bucket
-# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-#   bucket = aws_s3_bucket.terraform_state[0].bucket
+# Create encryption configuration for the S3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.bucket
 
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
 output "bucket_name" {
-  value = aws_s3_bucket.terraform_state[0].bucket
+  value = aws_s3_bucket.terraform_state.bucket
 }
