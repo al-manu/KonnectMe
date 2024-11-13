fece0175a9f651c0f25210536ec0a65e12175f9f
Ala Mani <ala.a.mani@capgemini.com>
2024-11-13 12:34:47 +0100

    13-11

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 # Call the S3 module/* 
-  module "s3_buckets" { 
-    source = "/modules/S3"  # Path to the S3 module
-  
-  } 
+module "s3_buckets" { 
+  source = "./modules/S3"  # Path to the S3 module
+}
---
+++ modules/S3/outputs.tf
@@ -0,0 +1,15 @@
+output "in_bucket_arn" {
+  value = aws_s3_bucket.in_bucket.arn  # ARN of the input bucket
+}
+
+output "out_bucket_arn" {
+  value = aws_s3_bucket.out_bucket.arn  # ARN of the output bucket.
+}
+
+output "tmp_bucket_arn" {
+  value = aws_s3_bucket.tmp_bucket.arn  # ARN of the temporary bucket.
+}
+
+output "export_bucket_arn" {
+  value = aws_s3_bucket.export_bucket.arn  # ARN of the export bucket.
+}
