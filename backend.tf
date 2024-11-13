39489c0c562c892c700f7b7dbf68cca3e571555c
Ala Mani <ala.a.mani@capgemini.com>
2024-11-13 21:01:20 +0100

    November 13

--- backend.tf
+++ backend.tf
@@ -1,12 +1,5 @@
-# backend.tf (optional, you can leave it empty if configuring via -backend-config)
-
 terraform {
-#   Do NOT define a backend block here if using -backend-config dynamically
-#   Example of leaving it empty:
   backend "s3" {
-    # bucket = "your-bucket"
-    # key     = "dev/terraform.tfstate"
-    # # region  = "us-west-1"
     encrypt = true                         # Enable encryption for state files
     acl     = "bucket-owner-full-control"   # Optional ACL setting for full control
   }
--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 module "s3_buckets" {
   source = "./modules/S3"  # Path to the S3 module
-
   # Pass the required variables, which will be read from dev.tfvars
   in_bucket_name     = var.in_bucket_name
   out_bucket_name    = var.out_bucket_name
