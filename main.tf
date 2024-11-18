5c725dc1e85f9c29607cc05201bb3e481c21fa70
Ala Mani <ala.a.mani@capgemini.com>
2024-11-18 15:26:11 +0100

    adding things

--- main.tf
+++ main.tf
@@ -21,8 +21,8 @@ module "vpc" {
   source = "./modules/vpc"
 
   cidr_block            = "10.0.0.0/16"
-  public_subnet_cidr    = "10.0.1.0/24"
-  private_subnet_cidr   = "10.0.2.0/24"
+  public_subnet_cidr    = "10.0.1.0/22"
+  private_subnet_cidr   = "10.0.2.0/22"
   public_subnet_az      = "eu-central-1a"
   private_subnet_az     = "eu-central-1b"
 }
