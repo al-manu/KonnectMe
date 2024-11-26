9b19f7a9ebe3f7df1e40067f23f48a5f9248217e
Ala Mani <ala.a.mani@capgemini.com>
2024-11-26 17:31:11 +0100

    redshift test

--- modules/redshift/main.tf
+++ modules/redshift/main.tf
@@ -232,7 +232,7 @@ resource "aws_lambda_function" "password_rotation" {
   runtime = "python3.8"
 
   # Add the Lambda function code
-  filename = "${path.module}/scripts/lambda/lambda.zip"  # Assuming you've packaged the code into a ZIP file
+  filename = "${path.module}/../../scripts/lambda/lambda.zip"  # Assuming you've packaged the code into a ZIP file
   source_code_hash = filebase64sha256("${path.module}/../../scripts/lambda/lambda.zip")
 
   environment {
