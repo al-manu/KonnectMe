4cf970b246e6a6b7a31c2e20dbd221afa5929fb0
Ala Mani <ala.a.mani@capgemini.com>
2024-11-26 12:05:59 +0100

    redshift test

--- modules/redshift/main.tf
+++ modules/redshift/main.tf
@@ -166,15 +166,13 @@ resource "aws_redshiftserverless_namespace" "redshift_namespace" {
 }
 
 # Create Redshift Workgroup
-
-resource "aws_redshiftserverless_workgroup" "redshift_serverless" {
-  workgroup_name         = var.workgroup_name
-  base_capacity          = var.base_capacity
-  enhanced_vpc_routing  = var.enhanced_vpc_routing
-  namespace_name        = aws_redshiftserverless_namespace.redshift_namespace.namespace_name
-  # vpc_security_group_ids = [aws_security_group.redshift.id]
-
-  tags = merge(var.tags, { "Name" = "${var.project_name}-redshift-workgroup" })
-
+resource "aws_redshiftserverless_workgroup" "redshift_workgroup" {
+  workgroup_name       = var.workgroup_name
+  namespace_name       = aws_redshiftserverless_namespace.redshift_namespace.namespace_name
+  base_capacity        = var.base_capacity
+  enhanced_vpc_routing = var.enhanced_vpc_routing
+  # subnet_ids           = aws_subnet.private[*].id
+  # security_group_ids   = [aws_security_group.redshift.id]
   depends_on = [aws_redshiftserverless_namespace.redshift_namespace]  # Ensure namespace is created first
+  tags = merge(var.tags, { "Name" = "${var.project_name}-redshift-workgroup" })
 }
